d67598a49a966e7bdd7a6f14ff44a2d5f3e68466
chenzhihao <1464208022@qq.com>
2021-03-15 10:01:51 +0800

    完成首页

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -11,13 +11,27 @@ $(function () {
 
     /* 拦截所有ajax请求 */
     // 处理参数
-    $.ajaxPrefilter(function (params) {
-        // console.log(params);
+    $.ajaxPrefilter(function (options) {
+        // console.log(options);
         // if ('http://127.0.0.1:5500' === '') {
         //     return;
         // }
         /* 拼接对应环境服务器地址 */
-        params.url = baseURL + params.url;
+        options.url = baseURL + options.url;
+        if (options.url.indexOf("/my/") !== -1) {
+            options.headers = {
+                Authorization: localStorage.getItem('token') || ''
+            };
 
+        }
+
+        options.complete = function (res) {
+            let obj = res.responseJSON;
+            console.log(obj);
+            if (obj.status == 1 && obj.message == '身份认证失败！') {
+                localStorage.removeItem('token');
+                location.href = '/login.html';
+            }
+        }
     })
 })
